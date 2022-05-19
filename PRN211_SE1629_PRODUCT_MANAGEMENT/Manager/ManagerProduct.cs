31b6b258507a280c2021c4fa2d698f5553ac4e5a
fishbone2710 <khanhvt2710@gmail.com>
2022-05-19 10:42:29 +0700

    bhgdh

--- PRN211_SE1629_PRODUCT_MANAGEMENT/Manager/ManagerProduct.cs
+++ PRN211_SE1629_PRODUCT_MANAGEMENT/Manager/ManagerProduct.cs
@@ -2,11 +2,67 @@
 public class ProductManagement:IProduct
 {
     private Product[] listProduct;
-
+    private int size; //So luong phan tu that trong mang 1 chieu listProduct
     public ProductManagement()
     {
-        this.listProduct = new Product[1];
+        this.size = 0;
+        this.listProduct = new Product[Utils.InitLengProduct];
     }
     
+    public ProductManagement(Product[] listProduct)
+    {
+        this.listProduct = listProduct;
+        foreach(Product p in listProduct)
+        {
+            if (p != null) this.size++;
+        }
+    }
+
+    public Product[] ListProduct { get => listProduct; set => listProduct = value; }
+
+    public void Add(Product p)
+    {
+        //Viet giai thuat de thuc thi chen 1 phan tu vao mang 1 chieu tinh
+        //1. Check de xem mang con trong khong ?
+        if(this.size >= this.listProduct.Length)
+        {
+            //Thuc hien noi mang
+            Product[] tmp = new Product[this.size*2];
+
+            //Hot du lieu tu mang cu sang mang moi
+            for(int i = 0; i < this.size; i++)
+            {
+                tmp[i] = this.listProduct[i];
+            }
+
+            this.listProduct = tmp;
+        }
+
+        //2. Neu ma con cho trong, thi chi add product
+        this.listProduct[this.size++] = p;
+    }
+
+    public bool Delete(Product p)
+    {
+        return true;
+    }
+
+    public void Display()
+    {
+        foreach(Product p in this.listProduct)
+        {
+            Console.WriteLine(p);
+        }
+    }
+
+    public Product GetProduct(int productID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(Product p)
+    {
+        throw new NotImplementedException();
+    }
 }
 
--- PRN211_SE1629_PRODUCT_MANAGEMENT/Program.cs
+++ PRN211_SE1629_PRODUCT_MANAGEMENT/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PRN.SE1629;
+using static System.Console;
+public class Program
+{
+    public static void Main()
+    {
+        //1. Khai bao doi tuong ManagementProduct
+        ProductManagement productManagement = new ProductManagement();
+
+        //2. Them n product
+        productManagement.Add(new Product(1, "Laptop Dell 15 inch", "New product with new features 2022", 1500, 100));
+        productManagement.Add(new Product(2, "Macbook Pro 2022", "New product with new features 2022", 2000, 200));
+        productManagement.Add(new Product(3, "Iphone 14 Pro", "New product with new features 2022", 2500, 300));
+        productManagement.Add(new Product(4, "Samsung Galaxy", "New product with new features 2022", 3000, 400));
+
+        productManagement.Display();
+    }
+}
