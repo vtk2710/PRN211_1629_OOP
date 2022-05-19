923816835ca9c81298e1ff8e0e124709f3f769a7
fishbone2710 <khanhvt2710@gmail.com>
2022-05-19 09:11:08 +0700

    Add project files.

---
+++ PRN211_1629_OOP/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace PRN_1629;
+public class Employee
+{
+    /*fields/data*/
+    private int _id;
+    private string _name;
+
+    /*properties*/
+    public double salary { get; set; } //.NET 5 +
+    public int Id { get => _id; set => _id = value; }
+    public string Name { get => _name; set => _name = value; }
+
+    /*constructors*/
+    public Employee()
+    {
+
+    }
+    public Employee(int id, string name, double salary)
+    {
+        Id = id;
+        Name = name;
+        this.salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return $"(ID = {this._id}, Name = {this._name}, Salary = {this.salary})";
+    }
+}
---
+++ PRN211_1629_OOP/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PRN_1629;
+
+using System.Collections;
+using static System.Console;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*Employee e = new Employee();
+        e.Id = 1;
+        e.Name = "Khanh";
+        e.salary = 150.55d;
+        Clear();
+        WriteLine(e);*/
+
+        ArrayList list = new ArrayList();
+        list.Add(new Employee(1, "a", 100));
+        list.Add(new Employee(2, "b", 200));
+        list.Add(new Employee(3, "c", 300));
+        list.Add(new Employee(4, "d", 400));
+        list.Add(new Employee(5, "e", 500));
+        list.Add(new Employee(6, "f", 600));
+
+        Display(list);
+    }
+
+    private static void Display(ArrayList list)
+    {
+        /*foreach(Employee e in list) {
+            WriteLine(e);
+        }*/
+
+        for (int i = 0; i < list.Count; i++) {
+            WriteLine(list[i]);
+        }
+    }
+}
