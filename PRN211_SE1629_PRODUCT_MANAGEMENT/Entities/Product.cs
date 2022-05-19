68620c8f183477a472b4d4dce425a11c9e318d85
fishbone2710 <khanhvt2710@gmail.com>
2022-05-19 10:07:17 +0700

    commit product project management first time

---
+++ PRN211_SE1629_PRODUCT_MANAGEMENT/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace PRN.SE1629;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string Desc { get; set; }
+    public double UnitPrice { get; set; }
+    public int Quantity { get; set; }
+
+    public Product() { }
+
+    public Product(int id, string productName, string desc, double unitPrice, int quantity)
+    {
+        Id = id;
+        ProductName = productName;
+        Desc = desc;
+        UnitPrice = unitPrice;
+        Quantity = quantity;
+    }
+    public override string ToString() => $"[ID = {Id}, Product Name = {ProductName}, Desc = {Desc}, Unit Price = {UnitPrice}, Quantity = {Quantity}";
+
+    /*Increase percent unit_price*/
+    public void IncreaseUnitPrice(double unitPrice) => UnitPrice = UnitPrice * unitPrice/100;
+}
---
+++ PRN211_SE1629_PRODUCT_MANAGEMENT/Manager/iProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace PRN.SE1629;
+
+public interface IProduct
+{
+    public abstract void Add(Product p);
+    public abstract bool Update(Product p);
+    public abstract bool Delete(Product p);
+    public abstract Product GetProduct(int productID);
+    void Display();
+}
